820c63c9d0dc29da7994350d560c16e28374a6c5
prayogaarchitects <prayogaarchitects@gmail.com>
2022-11-24 23:26:27 +0530

    added cdn jquery bootstrap

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,7 +13,7 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : 'Loading'}</p>
                 <a
-                  href='ourservices'
+                  href='/homepage/ourservices'
                   className='btn btn-custom btn-lg page-scroll'
                 >
                   Learn More
